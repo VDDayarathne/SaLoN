0a5180349ba355b9f67d234d80a3a8e7a9e882e0
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-03-18 02:22:20 +0530

    updated view_product in admin dashboard

--- resources/views/admin/view_product.blade.php
+++ resources/views/admin/view_product.blade.php
@@ -53,34 +53,36 @@
                         <th>Price</th>
                         <th>Quantity</th>
                         <th>Image</th>
+                        <th>Delete</th>
                     </tr>
                 @foreach($product as $products)
                     <tr>
                         <td>{{ $products->title }}</td>
-                        <td>{{ $products->description }}</td>
+                        <td>{!!Str::words($products->description,5)!!}</td>
                         <td>{{ $products->category }}</td>
                         <td>{{ $products->price }}</td>
                         <td>{{ $products->quantity }}</td>
                         <td>
                             <img height="100" width="100" src="products/{{ $products->image}}" alt="">
                         </td>
+                        <td>
+                            <a class="btn btn-danger" onclick="confirmation(event)" href="{{url('delete_product',$products->id)}}">Delete</a>
+                        </td>
+                        
                     </tr>
 
                 @endforeach
 
                 </table>
+
             </div> 
+            <div class="div_deg">
+                {{ $product->onEachSide(1) -> links() }}
+            </div>
+           
         </div>
       </div>
     </div>
-    <!-- JavaScript files-->
-    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
-    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
-    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
-    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
-    <script src="{{asset('admincss/js/front.js')}}"></script>
+    @include('admin.js')
   </body>
 </html>
