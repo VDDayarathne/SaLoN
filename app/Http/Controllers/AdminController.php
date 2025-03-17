98eb7fcba2e2b01a55bed814d21e2ac93d577705
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-03-17 19:19:52 +0530

    updated admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,6 +14,27 @@ class AdminController extends Controller
         return view('admin.category',compact('data'));
     }
 
+    public function add_category(request $request) 
+    {
+        $category = new Category;
+        $category->category_name = $request->category;
+        $category->save();
+
+        toastr()->timeOut(2000)->closeButton()->addSuccess('Category Added Succesfully.');
+
+        return redirect()->back();
+    }
+
+    public function delete_category($id)
+    {
+        $data = Category::find($id);
+
+        $data->delete();
+
+        toastr()->timeOut(2000)->closeButton()->addSuccess('Category Deleted Succesfully.');
+        return  redirect()->back();
+    }
+
     
 
 }
