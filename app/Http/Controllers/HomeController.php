fe90116c918e8193176ab929dbe5c48021a9f7c5
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-03-22 02:05:00 +0530

    updated User HomePage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+
+
 
 class HomeController extends Controller
 {
@@ -13,6 +16,13 @@ class HomeController extends Controller
 
     public function home() 
     {
-        return view('home.index');
+        $product = Product::all();
+        return view('home.index',compact('product'));
+    }
+
+    public function login_home()
+    {
+        $product = Product::all();
+        return view('home.index',compact('product'));
     }
 }
