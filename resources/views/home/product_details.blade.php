bfe31048040832ab55ccf027f93bd0a99c3f0147
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-03-25 19:58:20 +0530

    updated product deta

---
+++ resources/views/home/product_details.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  @include('home.css')
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header -->
+    @include('home.header')
+  </div>
+
+<br><br><br>
+
+    <!-- footer -->
+    @include('home.footer')
+  
+</body>
+
+</html>
