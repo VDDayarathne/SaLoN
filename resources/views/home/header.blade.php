aa2679d58b4d70f9e82a15430b6043e05f9862fb
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-03-26 22:15:50 +0530

    updated header

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -12,7 +12,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav  ">
           <li class="nav-item active">
-            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="shop.html">
@@ -56,12 +56,7 @@
           </a>
           @endauth
           @endif
-
           
-
-          <a href="">
-            <i class="fa fa-shopping-bag" aria-hidden="true"></i>
-          </a>
           <form class="form-inline ">
             <button class="btn nav_search-btn" type="submit">
               <i class="fa fa-search" aria-hidden="true"></i>
