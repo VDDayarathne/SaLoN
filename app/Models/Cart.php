1322255833235abb9a98d9774bf2392354ec0282
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-04-07 19:34:41 +0530

    added Cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Cart extends Model
 {
-    //
+    use HasFactory;
+    public function user()
+    {
+        return $this->hasOne('App\models\User','id','user_id');
+    }
+    public function product()
+    {
+        return $this->hasOne('App\models\Product','id','product_id');
+    }
 }
