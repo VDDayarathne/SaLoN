50bf172d92618d29485aed8b72159bf2a754b6fa
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-04-07 19:34:14 +0530

    my cart section

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,19 +21,50 @@ class HomeController extends Controller
     public function home() 
     {
         $product = Product::all();
-        return view('home.index',compact('product'));
+        if(Auth::id())
+        {
+            $user = Auth::user();
+            $userid = $user->id;
+            $count = Cart::where('user_id',$userid)->count();
+        }
+        else
+        {
+            $count = '';
+        }
+        
+        return view('home.index',compact('product','count'));
     }
 
     public function login_home()
     {
         $product = Product::all();
-        return view('home.index',compact('product'));
+        if(Auth::id())
+        {
+            $user = Auth::user();
+            $userid = $user->id;
+            $count = Cart::where('user_id',$userid)->count();
+        }
+        else
+        {
+            $count = '';
+        }
+        return view('home.index',compact('product','count'));
     }
 
     public function product_details($id)
     {
         $data = Product::find($id);
-        return view('home.product_details', compact('data'));
+        if(Auth::id())
+        {
+            $user = Auth::user();
+            $userid = $user->id;
+            $count = Cart::where('user_id',$userid)->count();
+        }
+        else
+        {
+            $count = '';
+        }
+        return view('home.product_details', compact('data','count'));
     }
 
     public function add_cart($id)
@@ -48,6 +79,18 @@ class HomeController extends Controller
         toastr()->timeOut(2000)->closeButton()->addSuccess('Product Added to the cart Succesfully.');
         
         return redirect()->back();
+    }
+
+    public function mycart()
+    {
+        if(Auth::id())
+        {
+            $user = Auth::user();
+            $userid = $user->id;
+            $count = Cart::where('user_id',$userid)->count();
+            $cart = cart::where('user_id', $userid)->get();
+        }
+        return view('home.mycart',compact('count','cart'));
     }
 
 }
