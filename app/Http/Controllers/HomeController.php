17f993c6cd8a2e4ab602ecaa0d625a24ea480bc0
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-04-22 00:30:20 +0530

    updated Home Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\User;
 use App\Models\Cart;
+use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -91,6 +92,45 @@ class HomeController extends Controller
             $cart = cart::where('user_id', $userid)->get();
         }
         return view('home.mycart',compact('count','cart'));
+    }
+
+    public function delete_cart($id)
+    {
+        $data = Cart::find($id);
+
+        $data->delete();
+
+        toastr()->timeOut(2000)->closeButton()->addSuccess('Product removed Succesfully.');
+        return  redirect()->back();
+    }
+
+    public function confirm_order(Request $request)
+    {
+        $name = $request->name;
+        $address = $request->address;
+        $phone = $request->phone;
+        $userid = Auth::user()->id;
+        $cart = Cart::where('user_id', $userid)->get();
+        foreach($cart as $carts)
+        {
+            $order = new Order;
+            $order->name = $name;
+            $order->rec_address = $address;
+            $order->phone = $phone;
+            $order->user_id = $userid;
+            $order->product_id = $carts->product_id;
+            $order->save();
+            
+        }
+
+        $cart_remove = Cart::where('user_id',$userid)->get();
+        foreach($cart_remove as $remove)
+        {
+            $data = cart::find($remove->id);
+            $data->delete();
+        }
+        toastr()->timeOut(2000)->closeButton()->addSuccess('Product Ordered Succesfully.');
+        return redirect()->back(); 
     }
 
 }
