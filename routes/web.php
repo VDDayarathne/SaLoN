61293616c9ef944f66a0b9d5cbb56ff389e59818
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-04-26 00:41:58 +0530

    Updated Routes

--- routes/web.php
+++ routes/web.php
@@ -82,3 +82,8 @@ middleware(['auth','admin']);
 route::get('delivered/{id}',[AdminController::class,'delivered'])->
 middleware(['auth','admin']);
 
+route::get('print_pdf/{id}',[AdminController::class,'print_pdf'])->
+middleware(['auth','admin']);
+
+route::get('myorders',[HomeController::class,'myorders'])->
+middleware(['auth','verified']);
