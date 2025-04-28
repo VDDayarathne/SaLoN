3370d0813af996e0dff530377f4c9ebd0a927ff0
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2025-04-28 11:11:16 +0530

    Updated HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,8 @@ use App\Models\User;
 use App\Models\Cart;
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
-
+use Stripe;
+use Session;
 
 
 
@@ -136,6 +137,66 @@ class HomeController extends Controller
         }
         toastr()->timeOut(2000)->closeButton()->addSuccess('Product Ordered Succesfully.');
         return redirect()->back(); 
+    }
+
+    public function myorders()
+    { 
+        $user = Auth::user()->id;
+        $count = Cart::where('user_id',$user)->get()->count();
+        $order = Order::where('user_id',$user)->get();
+        return view('home.order',compact('count','order'));
+    }
+
+    public function stripe($value)
+    {
+
+        return view('home.stripe',compact('value'));
+
+    }
+
+    public function stripePost(Request $request,$value)
+    {
+
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+
+    
+
+        Stripe\Charge::create ([
+
+                "amount" => $value * 100,
+                "currency" => "usd",
+                "source" => $request->stripeToken,
+                "description" => "Test payment from complete" 
+
+        ]);
+
+        $name = Auth::user()->name;
+        $phone = Auth::user()->phone;
+        $address = Auth::user()->address;
+        $userid = Auth::user()->id;
+        $cart = Cart::where('user_id', $userid)->get();
+        foreach($cart as $carts)
+        {
+            $order = new Order;
+            $order->name = $name;
+            $order->rec_address = $address;
+            $order->phone = $phone;
+            $order->user_id = $userid;
+            $order->product_id = $carts->product_id;
+            $order->payment_status="paid";
+            $order->save();
+            
+        }
+
+        $cart_remove = Cart::where('user_id',$userid)->get();
+        foreach($cart_remove as $remove)
+        {
+            $data = cart::find($remove->id);
+            $data->delete();
+        }
+        toastr()->timeOut(2000)->closeButton()->addSuccess('Product Ordered Succesfully.');
+        return redirect('mycart'); 
+
     }
 
 }
